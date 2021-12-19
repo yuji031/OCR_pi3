6dca0cc4949e9d57c7ca8d288018b252fa8e9301
yuji031 <69129481+yuji031@users.noreply.github.com>
2021-12-19 15:21:24 +0900

    Update ocr.py

--- ocr.py
+++ ocr.py
@@ -1,17 +1,17 @@
-from PIL import Image, ImageFont, ImageDraw
+from PIL import Image
 import pyocr
-import numpy as np
-
+import numpy
 
 tools = pyocr.get_available_tools()
 tool = tools[0]
 
-area = (217,100, 367,170)
+area = (218,97, 425,151)
 
+imgj = Image.open('C:\\programming\\SSH_programm\\data\\temp\\camera_capture.jpg')
 txt = tool.image_to_string(
-    Image.open('data/temp/camera_capture.jpg').crop(area),
+    img.crop(area),
     lang = "eng",
-    builder=pyocr.builders.TextBuilder()
+    builder = pyocr.builders.TextBuilder()
     )
 
 print(txt)
